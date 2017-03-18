7675fae97606c5f922ea02636853a1716f3e3f45
nnnn20430 <nnnn20430@mindcraft.si.eu.org>
2017-03-18 21:18:00 +0100

    build_index: new fancy version for modern browsers

---
+++ pkg/nginxprefix/html/apps/build_index/build_index.php
@@ -0,0 +1,160 @@
+<?php
+
+$indexStyle = <<<EOF
+body {
+	background-color: #121212;
+	color: #BBBBBB;
+	font-family: "Monospace", Monospace;
+	margin-top: 30px;
+}
+a {
+	color: #FFFFFF;
+	text-decoration: none;
+	font-weight: bold;
+	outline:0;
+}
+a:hover {
+	color: #00BFFF;
+	text-decoration: none;
+}
+a:active {
+	color: #FF3300;
+	text-decoration: none;
+	outline:0;
+}
+th {
+	font-size: 120%;
+	border-bottom-width: 3px;
+	border-bottom-style: solid;
+	border-bottom-color: #33FF00;
+}
+td {
+	padding-right: 30px;
+}
+EOF;
+
+function sizeFilter($bytes, $decimals = 2) {
+	$label = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB');
+	$factor = floor((strlen($bytes) - 1) / 3);
+	$filteredSize = $bytes/pow(1024, $factor);
+	$filteredSize = sprintf("%.{$decimals}f", $filteredSize);
+	$sizeString = $filteredSize . $label[$factor];
+	return $sizeString;
+}
+
+function filesGroupDir($dir, $array) {
+	$dirArr = [];
+	$fileArr = [];
+	foreach ($array as $k => $v) {
+		$path = getRealPath($dir.$v);
+		if(filetype($path) == 'dir') {
+			$dirArr = array_merge($dirArr, [$v]);
+		} else {
+			$fileArr = array_merge($fileArr, [$v]);
+		}
+	}
+	return array_merge($dirArr, $fileArr);
+}
+
+function getRealPath($path) {
+	$rpath = realpath($path);
+	if ($rpath === false) {
+		$escapedPath = escapeshellarg($path);
+		$execOutput = [];
+		$execReturn = 1;
+		@exec("realpath $escapedPath", $execOutput, $execReturn);
+		if ($execReturn === 0) {
+			$rpath = $execOutput[0];
+		}
+	}
+	return $rpath;
+}
+
+function getFileSize($path) {
+	$size = filesize($path);
+	if ($size === false) {
+		$escapedPath = escapeshellarg($path);
+		$execOutput = [];
+		$execReturn = 1;
+		@exec("stat -Lc%s $escapedPath", $execOutput, $execReturn);
+		if ($execReturn === 0) {
+			$size = $execOutput[0];
+		}
+	}
+	return $size;
+}
+
+function getFileMTime($path) {
+	$mtime = filemtime($path);
+	if ($mtime === false) {
+		$escapedPath = escapeshellarg($path);
+		$execOutput = [];
+		$execReturn = 1;
+		@exec("stat -Lc%Y $escapedPath", $execOutput, $execReturn);
+		if ($execReturn === 0) {
+			$mtime = $execOutput[0];
+		}
+	}
+	return $mtime;
+}
+
+function main() {
+	global $indexStyle;
+
+	$dir = $_SERVER['PATH_ROOT'] . $_SERVER['PATH_INFO'];
+	$files = array_diff(scandir($dir), array('.', '..'));
+	$files = filesGroupDir($dir, $files);
+
+	$html = "";
+
+	$html .= "<!DOCTYPE html>\n";
+	$html .= "<html>\n";
+	$html .= "<head>\n";
+	$html .= "<meta charset=\"UTF-8\">\n";
+	$html .= "<style>" . $indexStyle . "</style>\n";
+	$html .= "</head>\n";
+	$html .= "<body>\n";
+	$html .= "<h2>Index of " . $_SERVER['PATH_INFO'] . "</h2>\n";
+	$html .= "<table>\n";
+
+	$html .= "<tr>\n";
+	$html .= "<th>Name</th>\n";
+	$html .= "<th>Size</th>\n";
+	$html .= "<th>Time</th>\n";
+	$html .= "</tr>\n";
+
+	$html .= "<tr>\n";
+	$html .= '<td><a href="..">..</a></td>'."\n";
+	$html .= "<td></td>\n";
+	$html .= "<td></td>\n";
+	$html .= "</tr>\n";
+
+	foreach ($files as $k => $v) {
+		$name = $v;
+		if (strlen($name) > 30)
+			$name = substr($name, 0, 27) . '...';
+		$path = getRealPath($dir.$v);
+		$datemodified = date('Y\-m\-d H:i', getFileMTime($path));
+		$size = sizeFilter(getFileSize($path));
+		$hrefSuffix = '';
+		if(filetype($path) == 'dir') {
+			$hrefSuffix = '/';
+			$size = "-";
+		}
+		$html .= "<tr>\n";
+		$html .= '<td><a href="' . $v.$hrefSuffix . '">' . $name.$hrefSuffix . '</a></td>'."\n";
+		$html .= "<td>" . $size . "</td>\n";
+		$html .= "<td>" . $datemodified . "</td>\n";
+		$html .= "</tr>\n";
+	}
+
+	$html .= "</table>\n";
+	$html .= "</body>\n";
+	$html .= "</html>\n";
+
+	echo $html;
+}
+
+main();
+
+?>
---
+++ pkg/nginxprefix/html/apps/build_index/index.php
@@ -0,0 +1,20 @@
+<?php
+
+function get_browser_name($user_agent) {
+    if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
+    elseif (strpos($user_agent, 'Edge')) return 'Edge';
+    elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
+    elseif (strpos($user_agent, 'Safari')) return 'Safari';
+    elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
+    elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
+    
+    return 'Other';
+}
+
+if (get_browser_name($_SERVER['HTTP_USER_AGENT']) != "Other") {
+	require 'build_index_modern.php';
+} else {
+	require 'build_index.php';
+}
+
+?>
